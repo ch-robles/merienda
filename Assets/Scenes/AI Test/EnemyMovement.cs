be3a270aeebcf89e2ffc8379d73a742f52be4b73
megubits <raeneelisha.quingquing.cics@ust.edu.ph>
2025-04-27 23:06:19 +0800

    navmesh test
    bare min navmesh test scene + added the old model from the game to be tested

---
+++ Assets/Scenes/AI Test/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform Player;
+    public float UpdateRate = 0.1f;
+    private NavMeshAgent Agent;
+
+    private void Awake()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FollowTarget());
+        // animation start here
+    }
+
+    private IEnumerator FollowTarget()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(UpdateRate);
+        
+        while(enabled)
+        {
+            Agent.SetDestination(Player.transform.position);
+            yield return Wait;
+        }
+    }
+}
