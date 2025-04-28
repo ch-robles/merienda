21c1213beb718820fa91f7bdf74e65eb82e466f1
DaRealMashukun <96064356+DaRealMashukun@users.noreply.github.com>
2025-04-28 14:36:16 +0800

    Falling Platform Script

---
+++ Assets/Scripts/FallingPlatforms.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class FallingPlatforms : MonoBehaviour
+{
+    bool isFalling = false;
+    float downspeed = 0;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //if Player Tagged object collides with collider then fall
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(StartFallingWithDelay(1f)); // Adjust delay duration (1 second for my implentation) as needed
+        }
+    }
+
+
+     IEnumerator StartFallingWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        isFalling = true;
+        Destroy(gameObject, 10);
+    }
+
+    void Update() {
+       if(isFalling){
+        downspeed += Time.deltaTime/20;
+        transform.position = new Vector3(transform.position.x, 
+        transform.position.y-downspeed,
+        transform.position.z);
+       } 
+    }
+}
