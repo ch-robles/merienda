d74b6bfda78a04f84d91d9ee2c2b80e9d7152e4a
megubits <raeneelisha.quingquing.cics@ust.edu.ph>
2025-05-02 11:32:11 +0800

    saving scene changes xp

---
+++ Assets/ChaseCaller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChaseCaller : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other){
+        Debug.Log("Fall detected.");
+        SceneManager.LoadScene("HuntingGrounds");
+    }
+}
